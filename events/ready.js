2893e744826a4bd78dd698a02232c676cbb23238
MeliL <45405550+Melil0@users.noreply.github.com>
2019-10-07 15:42:30 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,9 +7,25 @@ var prefix = ayarlar.prefix;
 
 
 module.exports = client => {
-  console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("idle");
-  client.user.setGame(`Testing Bot | XiR`);
-  console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("dnd");
+  //idle = boşta
+  //dnd = rahatsız etmeyin
+  //online = çevrimiçi
+  console.log(`                                                                                                  {XiR DeVeLoPeR TeAm}                                                                    `)
+  client.user.setActivity(`${prefix}yardım | ${client.guilds.size} sunucu | ${client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Kullanıcıyı`, { type: "LISTENING"});
+  //LISTENING = DİNLİYOR
+  //WATCHING = İZLİYOR
+  //PLAYING = OYNUYOR 
+  console.log(`${client.user.username}: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
-//XiR
+/*
+__   __ _ ______     ______                     _                             _____                         
+\ \ / /(_)| ___ \    |  _  \                   | |                           |_   _|                        
+ \ V /  _ | |_/ /    | | | |  ___ __   __  ___ | |  ___   _ __    ___  _ __    | |    ___   __ _  _ __ ___  
+ /   \ | ||    /     | | | | / _ \\ \ / / / _ \| | / _ \ | '_ \  / _ \| '__|   | |   / _ \ / _` || '_ ` _ \ 
+/ /^\ \| || |\ \     | |/ / |  __/ \ V / |  __/| || (_) || |_) ||  __/| |      | |  |  __/| (_| || | | | | |
+\/   \/|_|\_| \_|    |___/   \___|  \_/   \___||_| \___/ | .__/  \___||_|      \_/   \___| \__,_||_| |_| |_|
+                                                         | |                                                
+                                                         |_|                                                
+*/
